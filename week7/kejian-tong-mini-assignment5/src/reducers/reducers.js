7764ba8ac99119039aac9c380886f16efb414576
kejiantong <tong.ke@northeastern.edu>
2021-11-02 15:37:43 -0700

    week7 folder

--- week7/kejian-tong-mini-assignment5/src/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import clickReducer from './clickReducer'
-import playReducer from './playReducer'
-
-export default combineReducers({
-    play: playReducer,
-    clickCount: clickReducer
-})
