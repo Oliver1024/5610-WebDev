aa838404301b7584e66c5e094426b78fab73793d
kejiantong <tong.ke@northeastern.edu>
2021-11-07 02:08:56 -0800

    finish wk7 ec

---
+++ week7_ec/src/Square.jsx
@@ -0,0 +1,28 @@
+import './Square.css';
+
+export default function () {
+    return <body>
+        <div class="grid-container">
+            <div class="box box-center">
+                <div class="box-hover">
+                </div>
+            </div>
+            <div class="box box-header">
+                <h1>Kejian Tong's Website</h1>
+            </div>
+            <div class="box box-side-left">
+                <div class="ul-container">
+                    <ul>
+                        <li><a href="#">Home Page</a></li>
+                        <li><a href="#">Photos</a></li>
+                        <li><a href="#">Secret Project</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box box-side-right">
+            </div>
+            <div class="box box-footer">
+            </div>
+        </div>
+    </body>
+}
